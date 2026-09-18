720202c34de20354ee8f063c97bad75d756bbf18
Galuis <15099895+Galuis@users.noreply.github.com>
2026-09-18 20:58:05 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.edge20pro.camerakeyprobe"
-        minSdk = 23
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
